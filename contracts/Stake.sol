c4f38ddab175121013ee15433c96b729b94fbc87
Piyakarn Nimmakulvirut (Jimmy) <jirakarnjim1@gmail.com>
2023-10-16 01:04:40 +0700

    feat: update latest transactions

--- contracts/Stake.sol
+++ contracts/Stake.sol
@@ -5,19 +5,26 @@ contract Stake {
     uint256 public middlePool; // Middle pool
     mapping(address => uint256) public _stakingBalance; // Staking balance for each address
 
-    /**
-     * Update deposit value
-     *
-     * @param _amount amount of deposit event
-     */
-    event Deposit(address _depositAddress, uint256 _amount);
+    struct Transaction {
+        address requestAddress;
+        string actionType;
+        uint256 amount;
+    }
+
+    Transaction[] public transactions;
 
     /**
-     * Update withdraw value
+     * Update latest transaction
      *
-     * @param _amount amount of withdraw event
+     * @param _requestAddress address of interacting user
+     * @param _actionType type of action, must be deposit or withdraw
+     * @param _amount amount of transaction event
      */
-    event Withdraw(address _withdrawAddress, uint256 _amount);
+    event UpdateLatestTransaction(
+        address _requestAddress,
+        string _actionType,
+        uint256 _amount
+    );
 
     /**
      * Set middle pool value to 0
@@ -35,7 +42,12 @@ contract Stake {
         _stakingBalance[msg.sender] += msg.value;
         middlePool += msg.value;
 
-        emit Deposit(msg.sender, msg.value);
+        Transaction memory latestTransaction;
+        latestTransaction = Transaction(msg.sender, "deposit", msg.value);
+
+        transactions.push(latestTransaction);
+
+        emit UpdateLatestTransaction(msg.sender, "deposit", msg.value);
     }
 
     /**
@@ -53,6 +65,32 @@ contract Stake {
         _stakingBalance[msg.sender] -= _amount;
         middlePool -= _amount;
 
-        emit Withdraw(msg.sender, _amount);
+        Transaction memory latestTransaction;
+        latestTransaction = Transaction(msg.sender, "withdraw", _amount);
+
+        transactions.push(latestTransaction);
+
+        emit UpdateLatestTransaction(msg.sender, "withdraw", _amount);
+    }
+
+    function getLatestFiveTransactions()
+        public
+        view
+        returns (Transaction[] memory)
+    {
+        uint256 length = transactions.length;
+        uint256 count = 5;
+
+        // Ensure we don't try to get more elements than are available
+        if (count > length) {
+            count = length;
+        }
+
+        Transaction[] memory result = new Transaction[](count);
+        for (uint256 i = 0; i < count; i++) {
+            result[i] = transactions[length - count + i];
+        }
+
+        return result;
     }
 }
